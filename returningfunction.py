78a5cd17e31584ad24810865c361d227769ba97e
vandana1511 <59770185+vandana1511@users.noreply.github.com>
2020-06-25 23:04:26 +0530

    Create returningfunction.py

---
+++ returningfunction.py
@@ -0,0 +1,12 @@
+def hello(name='Jose'):
+    
+    def greet():
+        return '\t This is inside the greet() function'
+    
+    def welcome():
+        return "\t This is inside the welcome() function"
+    
+    if name == 'Jose':
+        return greet
+    else:
+        return welcome
