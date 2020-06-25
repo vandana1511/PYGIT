8cda4e87f8c758274af4bb52e832d014c2181fbb
vandana1511 <59770185+vandana1511@users.noreply.github.com>
2020-06-25 20:54:43 +0530

    created a new file

---
+++ functionInFunction.py
@@ -0,0 +1,12 @@
+def hello(name='Jose'):
+    print('The hello() function has been executed')
+    
+    def greet():
+        return '\t This is inside the greet() function'
+    
+    def welcome():
+        return "\t This is inside the welcome() function"
+    
+    print(greet())
+    print(welcome())
+    print("Now we are back inside the hello() function")
